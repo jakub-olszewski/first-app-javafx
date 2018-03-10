63db69c8024e505be51a33af78d5c6f9a6ea3327
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-03-10 11:12:04 +0100

    Rysowanie glowy ludzika

--- src/main/java/eu/b24u/gra/elementy/Ludzik.java
+++ src/main/java/eu/b24u/gra/elementy/Ludzik.java
@@ -1,10 +1,16 @@
 package eu.b24u.gra.elementy;
 
 import eu.b24u.javafx.Plotno;
+import eu.b24u.javafx.element.Punkt;
 
 public class Ludzik {
-
-	public Ludzik(Plotno plotno) {
+	/**
+	 * Konstruktor to jest metoda,która konstruuje,tworzy,buduje ludzika
+	 * 
+	 * @param plotno
+	 * @param punktRysowaniaLudzika
+	 */
+	public Ludzik(Plotno plotno, Punkt punktRysowaniaLudzika) {
 		// TODO Auto-generated constructor stub
 
 	}
